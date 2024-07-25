4b1878f6aa193c8e350cea72223a118df1efbd99
Kobe Amoh <kobexamoh@Kobes-MBP.lan>
2024-07-24 22:43:10 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// variables and constants
+const apiKEY = ""; // replace with your own API key
+let baseURL = "https://v3.football.api-sports.io/leagues?id=4";
+
+// Headers
+let requestHeaders = new Headers();
+requestHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+requestHeaders.append("x-rapidapi-key", apiKEY);
+
+
+
+let response = await fetch(baseURL, {
+    "method": "GET",
+    "headers": requestHeaders
+});
+
+if (response.ok) {
+    let results = await response.json();
+    console.log(results);
+} else {
+    alert("HTTP-Error: " + response.status);
+}
