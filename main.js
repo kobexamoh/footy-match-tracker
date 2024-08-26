0b856bfac3b185fa93d5afe9b6b01457b5c69764
Kobe Amoh <kobexamoh@gmail.com>
2024-08-26 00:03:45 -0600

    Begin logic for card generation on submit

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ import {generatePlayer} from "./generateplayer.js";
 
 let testPlayer = document.querySelector("#playerSearch")
 
+let playerCardBox = document.querySelector('.player-card-box')
+
+let findPlayer = document.querySelector('#playerLookup')
+
+console.log(playerCardBox)
+console.log(testPlayer)
+console.log(findPlayer)
+
+// on submit, get the player name
+// TODO: create the box in index.html
+    // TODO: then style the box
+        // TODO: on submit, generate the box
+
 testPlayer = testPlayer.value
+
+// on submit, get the player's name
+
